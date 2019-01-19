9a8469dc9ab18c00329a9d2cce7dc4023b569d97
Artem Khvastunov <art.spb@gmail.com>
2019-01-19 14:56:29 +0100

    Initial commit

---
+++ src/main/java/me/artspb/example/gradle/project/Main.java
@@ -0,0 +1,7 @@
+package me.artspb.example.gradle.project;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ src/test/java/me/artspb/example/gradle/project/MainTest.java
@@ -0,0 +1,10 @@
+package me.artspb.example.gradle.project;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+    }
+}
